d1cd482e490c86eb8d7e3783dcf12b10eb1bc72a
Sathvik <Sathvik.Rao@iiitb.ac.in>
2024-02-24 03:40:51 +0530

    More changes to handleQuery.py

--- TextAndImageProcessing/handlingQuery.py
+++ TextAndImageProcessing/handlingQuery.py
@@ -1,13 +1,11 @@
 import requests,json,os
 from dotenv import load_dotenv
-from subcategoriesClassification import classify
 def HandleQuery(query:str):
     load_dotenv()
-    PALM_KEY=os.environ.get('API_KEY')
+    PALM_KEY=os.environ.get('PALM_KEY')
     endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + PALM_KEY
     headers = {"Content-Type": "application/json"}
     prompt=f'''"Query:{query}\nIs the query related to medical lab report?. Output is Yes or No'''
-        
     data = {"contents": [{"parts": [{"text": prompt}]}]}
     response=requests.post(endpoint,headers=headers,json=data)
     if response.status_code!=200:
@@ -31,18 +29,27 @@ def HandleQuery(query:str):
                 13m. Vitamins\n\
                 13n. Urine Analysis\n\
                 13o. sCK-MB and cTN\n'''
-            templates='''12. Give details about the last lab test
+            
+            templates='''12. Give details about the last lab test\n
             13. Classify based on report type
             '''
+
             prompt=f"Templates:{templates}\n. Output: Return the indices of the templates which are the most similar to the query:{query}."
             data = {"contents": [{"parts": [{"text": prompt}]}]}
             response=requests.post(endpoint,headers=headers,json=data)
             result=response.json()["candidates"][0]["content"]["parts"][0]["text"]
+            if result=="12":
+                result=["12"]
             if('13' in result):
-                result=classify(query,categories)
-            with open("./TextAndImageProcessing/JSON/output.json","wb") as fh:
-                print(result)
-                json.dump(["Lab","FIND MANY",result],fh)
+                prompt1=f"Categories:{categories}\n Output:Strictly Return only the list of indices of the categories which are similar to the query:{query}."
+                data = {"contents": [{"parts": [{"text": prompt1}]}]}
+                response=requests.post(endpoint,headers=headers,json=data)
+                if response.status_code!=200:
+                    raise Exception("Could not communicate with api")
+                result=response.json()["candidates"][0]["content"]["parts"][0]["text"]
+            with open("./TextAndImageProcessing/JSON/output.json","w") as fh:
+                
+                json.dump({0:"Lab",1:"FIND MANY",2:result},fh)
         else:
             
             templates='''
@@ -58,18 +65,19 @@ def HandleQuery(query:str):
             10. Hello, how are you?
             11. Thank you'''
 
-            prompt=f"Templates:{templates}\n. Output: Return the indices of the templates which are the most similar to the query:{query}."
+            prompt=f"Templates:{templates}\n. Output:Strictly Return only the indices of the templates which are the most similar to the query:{query} as a list."
             
             data = {"contents": [{"parts": [{"text": prompt}]}]}
             response=requests.post(endpoint,headers=headers,json=data)
+            result=response.json()["candidates"][0]["content"]["parts"][0]["text"]
             
-            with open("./TextAndImageProcessing/JSON/output.json","wb") as fh:
-                json.dump(["Prescription","FIND MANY",response.json()["candidates"][0]["content"]["parts"][0]["text"]],fh)
+            with open("./TextAndImageProcessing/JSON/output.json","w") as fh:
+                json.dump({0:"Prescription",1:"FIND MANY",2:result},fh)
     
     except Exception as e:
         print(e)
         return None
 if __name__=="__main__":
-    print(HandleQuery("Give me my blood report"))
+    print(HandleQuery("Give me the blood report"))
     
 
