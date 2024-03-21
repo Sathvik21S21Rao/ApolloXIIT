69c475f8ed6ef76703f686aed441e2bd9820ff7b
Pushkar S <spushkar0071234@gmail.com>
2024-03-21 23:28:07 +0530

    Made basic callbacks

--- whatsapp server api/routes/post.js
+++ whatsapp server api/routes/post.js
@@ -1,6 +1,7 @@
 require('dotenv').config({
    path: ".env.local"
 })
+const fs = require('fs');
 
 const express = require('express');
 const { MessagingResponse } = require('twilio').twiml;
@@ -23,9 +24,24 @@ router.post('/', async (req, res, next) => {
 
       llmFunctions.Feature_Extraction(accessibleUrl, (result) => {
          console.log(result);
+         const output = fs.readFileSync('./TextAndImageProcessing/JSON/output.json');
+         const data = JSON.parse(output);
+         console.log("Feature_Extraction data:", data);
       });      
    } else {
-      message = new MessagingResponse().message("yeah you can send a report");
+      // message = new MessagingResponse().message("yeah you can send a report");
+      llmFunctions.Translate_Text(body.Body, (result) => {
+         console.log("Translate_Text Result:", result);
+         const translated = fs.readFileSync('./TextAndImageProcessing/JSON/Translate_output.json');
+         const translatedData = JSON.parse(translated);
+         console.log("Translated text:", translatedData);
+         llmFunctions.Make_Query(result, (result) => {
+            console.log("Make_Query result:", result);
+            const output = fs.readFileSync('./TextAndImageProcessing/JSON/output.json');
+            const data = JSON.parse(output);
+            console.log("Query data:", data);
+         });
+      });
    }
 
    res.set('Content-Type', 'text/xml');
