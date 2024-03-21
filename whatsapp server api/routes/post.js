eb352a3ba5f56060ff5df15e277da04d86a7d5bb
rahul <rahul.baradol.14@gmail.com>
2024-03-21 10:47:04 +0530

    basic image trial

--- whatsapp server api/routes/post.js
+++ whatsapp server api/routes/post.js
@@ -2,7 +2,6 @@ require('dotenv').config({
    path: ".env.local"
 })
 
-
 const express = require('express');
 const { MessagingResponse } = require('twilio').twiml;
 
@@ -11,21 +10,20 @@ const router = express.Router();
 const username = process.env.twilioAccountSid;
 const password = process.env.twilioAuthToken;
 
+const llmFunctions = require('./functioncalls');
+
 // const database = require('./database');
 
 router.post('/', async (req, res, next) => {
    const { body } = req;
 
-   // console.log(body);
-
    if (body.NumMedia > 0) {
       const url = body[`MediaUrl0`]
       const accessibleUrl = "https://" + username + ":" + password + "@" + url.slice(8);
 
-      console.log(accessibleUrl);
-
-      message = new MessagingResponse().message("Got the report!");
-      message.media(accessibleUrl);
+      llmFunctions.Feature_Extraction(accessibleUrl, (result) => {
+         console.log(result);
+      });      
    } else {
       message = new MessagingResponse().message("yeah you can send a report");
    }
